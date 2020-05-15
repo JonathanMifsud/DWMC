ff8d9961915ec0b006416ed158ff8d2b9031d34b
Jonathan Mifsud <jonathan.mifsud@Outlook.com>
2020-05-15 19:43:16 +1000

    Initial Project
    Created initial Unity project files

---
+++ Dude Where's My Coffee/Assets/Scripts/MenuNavigation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuNavigation : MonoBehaviour
+{
+    private Button level1button;
+    private Button level2button;
+    private Button level3button;
+    private Button Tutorialbutton;
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1"); //Moves scene to level 1
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2"); //Moves scene to level 2
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3"); //Moves scene to level 3
+    }
+    public void TutorialLevel()
+    {
+        SceneManager.LoadScene("TutorialLevel"); //Moves scene to level 4
+    }
+}
---
+++ Dude Where's My Coffee/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; //Movement Speed
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyInput();
+    }
+    void KeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //Player Jumps
+            //Vector2.up
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //Player moves right
+            //Vector2.right
+        }
+    }
+}
