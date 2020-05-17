3835b98d55a1b76ed26d4e0bb748d6d625d263f2
Jonathan Mifsud <jonathan.mifsud@Outlook.com>
2020-05-17 16:02:06 +1000

    Movement Controls
    Movement controls work on player object. Gravity and Jump successful. Camera moves with the player

--- Dude Where's My Coffee/Assets/Scripts/PlayerMovement.cs
+++ Dude Where's My Coffee/Assets/Scripts/PlayerMovement.cs
@@ -6,35 +6,68 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed; //Movement Speed
     private Rigidbody2D rb;
-    private Vector2 moveVelocity;
+
+    public float jumpUp; //Jump Speed - Vertical increase
+    public float jumpFall; //Fall Speed - Vertical decrease
+    public float lowJumpFall; //Fall from a small jump.
+
+    bool isGrounded = false; //Checks if character is on the ground. True on ground, false in air.
+    public Transform isGroundedChecker; //Empty object below player
+    public float checkGroundRadius; //Radius between ground and player
+    public LayerMask groundLayer; //Layer considered as Ground to the player
+    public float rememberGroundedFor; //Player stays grounded
+    float lastTimeGrounded; //Last time player on ground
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical")); //Left Arrow & Right Arrow Keys
-        moveVelocity = moveInput * speed; 
-        //KeyInput();
+        Move();
+        Jump();
+        SmootherJump();
+        CheckIfGrounded();
     }
-    private void FixedUpdate()
+    void Move()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        float x = Input.GetAxisRaw("Horizontal"); //If the player selects LEFT/A or RIGHT/D keys
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
     }
-    void KeyInput()
+    void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor)) //If the player selects the SPACE key
         {
-            //Player Jumps
-            //Vector2.up
+            rb.velocity = new Vector2(rb.velocity.x, jumpUp);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+    }
+    void SmootherJump()
+    {
+        if (rb.velocity.y < 0)
         {
-            //Player moves right
-            //Vector2.right
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (jumpFall - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpFall - 1) * Time.deltaTime;
+        }
+    }
+    void CheckIfGrounded()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null) //If player has collided with an object
+        {
+            isGrounded = true; //Player on ground
+        } else
+        {
+            if (isGrounded)
+            {
+                lastTimeGrounded = Time.time;
+            }
+            isGrounded = false; //Player not on ground
         }
     }
 }
