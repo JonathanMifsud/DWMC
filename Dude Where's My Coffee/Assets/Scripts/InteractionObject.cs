7fec8a1f045cdc8102ac3557f21e1fd9f8c3e896
Jansten Arzadon <jheinzar@gmail.com>
2020-06-07 16:15:20 +1000

    Implemented the ability to pick up items, added a key for testing and changed ground of the level to tilemap.

---
+++ Dude Where's My Coffee/Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    void DoInteraction()
+    {
+        // Picked up item and placed in inventory
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dude Where's My Coffee/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public GameObject currentInteractObj = null;
+
+    void Update()
+    {
+        if (Input.GetButtonDown ("Interact") && currentInteractObj)
+        {
+            currentInteractObj.SendMessage("DoInteraction");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag ("Interactable"))
+        {
+            Debug.Log(other.name);
+            currentInteractObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag ("Interactable"))
+        {
+            if (other.gameObject == currentInteractObj)
+            {
+                currentInteractObj = null;
+            }
+        }
+    }
+
+}
